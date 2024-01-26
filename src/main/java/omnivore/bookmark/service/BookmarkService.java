1c2a534144ddc31295f1b50146f9faea3ac8a14e
Brilly-Bohyun <bohyun564@swu.ac.kr>
2024-01-26 12:17:23 +0900

    feat: 북마크 등록 service 로직 생성
    Related to: #1

--- src/main/java/omnivore/bookmark/service/BookmarkService.java
+++ src/main/java/omnivore/bookmark/service/BookmarkService.java
@@ -2,6 +2,11 @@ package omnivore.bookmark.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import omnivore.bookmark.entity.Bookmark;
+import omnivore.bookmark.entity.User;
+import omnivore.bookmark.repository.BookmarkRepository;
+import omnivore.bookmark.repository.UserRepository;
+import org.bson.types.ObjectId;
 import org.json.JSONObject;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,6 +18,20 @@ import java.util.Base64;
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class BookmarkService {
+    private final BookmarkRepository bookmarkRepository;
+    private final UserRepository userRepository;
+
+    @Transactional
+    public void register (String jwt, String restaurantId) {
+        String userEmail = getEmailFromPayload(parseJwtPayload(jwt));
+        User user = getUserByEmail(userEmail);
+        ObjectId obj_restaurantId = new ObjectId(restaurantId);
+        bookmarkRepository.save(Bookmark.builder().userId(user.getId()).restaurantId(obj_restaurantId).build());
+    }
+
+    private User getUserByEmail(String email) {
+        return userRepository.findByEmail(email).orElseThrow(() -> new IllegalArgumentException("해당 사용자를 찾을 수 없습니다."));
+    }
 
     private String parseJwtPayload(String jwt) {
         String base64Payload = jwt.split("\\.")[1];
